f36cfaf197426c92ed9eee643d7f8fc135b1c403
Oram <164499760+ORAM15@users.noreply.github.com>
2026-08-23 16:31:25 +0530

    chore: expose retrieval primitives

--- src/groundtruth/retrieval/__init__.py
+++ src/groundtruth/retrieval/__init__.py
@@ -1,1 +1,15 @@
-__all__ = []
+from .dense import DenseRetriever, EmbeddingProvider, cosine_similarity
+from .hybrid import HybridRetriever
+from .lexical import LexicalRetriever
+from .models import RetrievalResult, Retriever, reciprocal_rank_fusion
+
+__all__ = [
+    "DenseRetriever",
+    "EmbeddingProvider",
+    "HybridRetriever",
+    "LexicalRetriever",
+    "RetrievalResult",
+    "Retriever",
+    "cosine_similarity",
+    "reciprocal_rank_fusion",
+]
